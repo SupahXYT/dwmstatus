857f9bd48a87bc4d4269415d54877262d5721ba5
SupahXYT <josuefirelol@gmail.com>
2022-01-04 12:23:02 -0800

    fuck it no more signal stirng

--- include/dwmstatus.c
+++ include/dwmstatus.c
@@ -3,7 +3,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
 static Display *dpy;
 
-char *
+char *mktimes(size_t size, char *buf) {
+  time_t s = time(NULL);
+  struct tm *tms = localtime(&s);
+
+  strftime(buf, size, "%H:%M %a %b %d", tms);
+  return buf;
+}
+
+int main(void) {
+  Display *dpy = XOpenDisplay(NULL);
+  if(
+}
